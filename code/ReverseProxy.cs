251b40761103e0443180eacd71ba03a7c968b797
Paul Johnston <paj@pajhome.org.uk>
2008-08-24 06:38:21 +0000

    Pass user-agent; allow back-end redirects without full URL; only include ? in URL when query string is present; present more useful error message when back-end site down

--- code/ReverseProxy.cs
+++ code/ReverseProxy.cs
@@ -8,11 +8,11 @@ using System.Diagnostics;
 
 namespace ReverseProxy
 {
-	public class ReverseProxy: IHttpHandler
-	{	
-	    EventLog event_log;	    
-	    public ReverseProxy()
-	    {
+    public class ReverseProxy: IHttpHandler
+    {   
+        EventLog event_log;     
+        public ReverseProxy()
+        {
             // Create the event logging stream
             if(ConfigurationSettings.AppSettings.Get("traceRedirect") != null)
             {
@@ -23,17 +23,20 @@ namespace ReverseProxy
                 event_log = new EventLog();
                 event_log.Source = source;
             }
-	    }
-	    
-		public void ProcessRequest(HttpContext context)
-		{			    		
-			// Create the web request to communicate with the back-end site
-			string remoteUrl = ConfigurationSettings.AppSettings["ProxyUrl"] + 
-			        context.Request.Path + "?" + context.Request.QueryString;
-			HttpWebRequest request = (HttpWebRequest) WebRequest.Create(remoteUrl);
-			request.AllowAutoRedirect = false;
-			request.Method = context.Request.HttpMethod;
-			request.ContentType = context.Request.ContentType;
+        }
+        
+        public void ProcessRequest(HttpContext context)
+        {                       
+            // Create the web request to communicate with the back-end site
+            string remoteUrl = ConfigurationSettings.AppSettings["ProxyUrl"] + 
+                    context.Request.Path;
+            if (context.Request.QueryString.ToString() != "")
+                remoteUrl += "?" + context.Request.QueryString;
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(remoteUrl);
+            request.AllowAutoRedirect = false;
+            request.Method = context.Request.HttpMethod;
+            request.ContentType = context.Request.ContentType;
+            request.UserAgent = context.Request.UserAgent;
             request.Headers["Remote-User"] = HttpContext.Current.User.Identity.Name;
             foreach(String each in context.Request.Headers)
                 if (!WebHeaderCollection.IsRestricted(each) && each != "Remote-User") 
@@ -45,16 +48,22 @@ namespace ReverseProxy
                 outputStream.Close();
             }
 
-			HttpWebResponse response;
-			try
-			{
-				response = (HttpWebResponse) request.GetResponse();
-			}
-			catch(System.Net.WebException we)
-			{
-			    response = (HttpWebResponse) we.Response;
-			    // TBD: handle case where we.Response is null
-			}
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse) request.GetResponse();
+            }
+            catch(System.Net.WebException we)
+            {
+                response = (HttpWebResponse) we.Response;
+                if(response == null)
+                {
+                    context.Response.StatusCode = 13;
+                    context.Response.Write("Could not contact back-end site");
+                    context.Response.End();
+                    return;
+                }
+            }
 
             // Copy response from server back to client
             context.Response.StatusCode = (int) response.StatusCode;
@@ -63,17 +72,19 @@ namespace ReverseProxy
             {   
                 if(ConfigurationSettings.AppSettings.Get("traceRedirect") != null)
                     event_log.WriteEntry("Back-end redirecting to: " + response.Headers.Get("Location"), EventLogEntryType.Information);
-                context.Response.AddHeader("Location", context.Request.Url.GetLeftPart(UriPartial.Authority) +
-                    response.Headers.Get("Location").Substring(ConfigurationSettings.AppSettings["ProxyUrl"].Length));
+                string urlSuffix = response.Headers.Get("Location");
+                if(urlSuffix.ToLower().StartsWith(ConfigurationSettings.AppSettings["ProxyUrl"].ToLower()))
+                    urlSuffix = urlSuffix.Substring(ConfigurationSettings.AppSettings["ProxyUrl"].Length);
+                context.Response.AddHeader("Location", context.Request.Url.GetLeftPart(UriPartial.Authority) + urlSuffix);
             }
             foreach(String each in response.Headers)
                 if(each != "Location")
                     context.Response.AddHeader(each, response.Headers.Get(each));           
-			CopyStream(response.GetResponseStream(), context.Response.OutputStream);
-			response.Close();
-			context.Response.End();
-		}
-		
+            CopyStream(response.GetResponseStream(), context.Response.OutputStream);
+            response.Close();
+            context.Response.End();
+        }
+        
         static public void CopyStream(Stream input, Stream output) {
             Byte[] buffer = new byte[1024];
             int bytes = 0;
@@ -81,12 +92,12 @@ namespace ReverseProxy
                 output.Write(buffer, 0, bytes);
         }
  
-		public bool IsReusable
-		{
-			get
-			{
-				return true;
-			}
-		}	
-	}
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }   
+    }
 }
