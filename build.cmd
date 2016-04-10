5b6b20b467a725342a3ed1e58dc63b4cf5d1b155
Konstantin Veretennicov <kveretennicov+github@gmail.com>
2016-04-10 13:29:19 +0200

    Switch build to .NET Framework 3.5

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-@c:\WINDOWS\Microsoft.NET\Framework\v1.1.4322\csc /nologo /debug+ /t:library /define:TRACE /out:bin\ReverseProxy.dll code\ReverseProxy.cs
+@c:\WINDOWS\Microsoft.NET\Framework\v3.5\csc /nologo /debug+ /t:library /define:TRACE /out:bin\ReverseProxy.dll code\ReverseProxy.cs
