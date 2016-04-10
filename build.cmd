36a8609bf500ed155f041c0de567b5a4d79a54de
Konstantin Veretennicov <kveretennicov@gmail.com>
2016-04-10 13:58:52 +0200

    Build into "out" directory

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-@c:\WINDOWS\Microsoft.NET\Framework\v3.5\csc /nologo /debug+ /t:library /define:TRACE /out:bin\ReverseProxy.dll code\ReverseProxy.cs
+if not exist out (md out)
+c:\WINDOWS\Microsoft.NET\Framework\v3.5\csc /nologo /debug+ /t:library /define:TRACE /out:out\ReverseProxy.dll code\ReverseProxy.cs
